fb2e990b4ad5c763a063498eca9ad8cad467a09f
“AyushAgrawal-04” <ayushmagrawal.76@gmail.com>
2025-06-26 11:06:48 +0530

    change api for openweathermap.com to weatherapi.com

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ window.onload = function () {
   }
 
   function getCityName(location) {
-    const apiKey = "58f9100d4178179df67ffaa88471a425"; // Replace with your OpenWeather API key
+    const apiKey = "58f9100d4178179df67ffaa88471a425";
     const url = `https://api.openweathermap.org/geo/1.0/reverse?lat=${location.lat}&lon=${location.lon}&limit=1&appid=${apiKey}`;
 
     fetch(url)
@@ -31,42 +31,94 @@ window.onload = function () {
         document.querySelector(".error").style.display = "none";
         document.querySelector(".location").style.display = "none";
         document.querySelector(".weather").style.display = "block";
-        // You can now call your weather API using this city name
         checkWeather(city);
       })
       .catch((error) => console.error("Error fetching city name:", error));
   }
 };
-const apiKey = "58f9100d4178179df67ffaa88471a425";
-const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiKey = "cdcb77cef8954e15ab0193322252506";
+const apiUrl = "http://api.weatherapi.com/v1/current.json?&aqi=no&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 async function checkWeather(city) {
-  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-  if (response.status == 404) {
+  const response = await fetch(apiUrl + city + `&key=${apiKey}`);
+  if (response.status == 400) {
     document.querySelector(".error").style.display = "block";
     document.querySelector(".location").style.display = "none";
     document.querySelector(".weather").style.display = "none";
   }
   var data = await response.json();
   console.log(data);
-  document.querySelector(".city").innerHTML = data.name;
-  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-  document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
-  if (data.weather[0].main == "Clouds") {
+  const weatherCondition = data.current.condition.code;
+  console.log(data.current.condition.code);
+  document.querySelector(".city").innerHTML = data.location.name;
+  document.querySelector(".temp").innerHTML =
+    Math.round(data.current.temp_c) + "°C";
+  document.querySelector(".humidity").innerHTML = data.current.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.current.wind_kph + "km/h";
+  if (
+    weatherCondition == 1003 ||
+    weatherCondition == 1006 ||
+    weatherCondition == 1009 ||
+    weatherCondition == 1063 ||
+    weatherCondition == 1087
+  ) {
     weatherIcon.src = "images/clouds.png";
-  } else if (data.weather[0].main == "Clear") {
+  } else if (weatherCondition == 1000) {
     weatherIcon.src = "images/clear.png";
-  } else if (data.weather[0].main == "Rain") {
+  } else if (
+    weatherCondition == 1240 ||
+    weatherCondition == 1243 ||
+    weatherCondition == 1246 ||
+    weatherCondition == 1273 ||
+    weatherCondition == 1276 ||
+    weatherCondition == 1180 ||
+    weatherCondition == 1183 ||
+    weatherCondition == 1186 ||
+    weatherCondition == 1189 ||
+    weatherCondition == 1192 ||
+    weatherCondition == 1195 ||
+    weatherCondition == 1198 ||
+    weatherCondition == 1201
+  ) {
     weatherIcon.src = "images/rain.png";
-  } else if (data.weather[0].main == "Drizzle") {
+  } else if (
+    weatherCondition == 1072 ||
+    weatherCondition == 1150 ||
+    weatherCondition == 1153 ||
+    weatherCondition == 1168 ||
+    weatherCondition == 1171
+  ) {
     weatherIcon.src = "images/drizzle.png";
-  } else if (data.weather[0].main == "Mist") {
+  } else if (
+    weatherCondition == 1030 ||
+    weatherCondition == 1135 ||
+    weatherCondition == 1147
+  ) {
     weatherIcon.src = "images/Mist.png";
-  } else if (data.weather[0].main == "Snow") {
+  } else if (
+    weatherCondition == 1066 ||
+    weatherCondition == 1069 ||
+    weatherCondition == 1114 ||
+    weatherCondition == 1117 ||
+    weatherCondition == 1204 ||
+    weatherCondition == 1207 ||
+    weatherCondition == 1210 ||
+    weatherCondition == 1213 ||
+    weatherCondition == 1219 ||
+    weatherCondition == 1222 ||
+    weatherCondition == 1225 ||
+    weatherCondition == 1237 ||
+    weatherCondition == 1249 ||
+    weatherCondition == 1252 ||
+    weatherCondition == 1255 ||
+    weatherCondition == 1258 ||
+    weatherCondition == 1261 ||
+    weatherCondition == 1264 ||
+    weatherCondition == 1279 ||
+    weatherCondition == 1282
+  ) {
     weatherIcon.src = "images/snow.png";
   }
   document.querySelector(".location").style.display = "none";
